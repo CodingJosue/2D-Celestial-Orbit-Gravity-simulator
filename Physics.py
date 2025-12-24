e145aa4d1eaadf8d561b05741548a39a6cf37825
CodingJosue <holyboom4@gmail.com>
2025-12-24 10:27:43 -0500

    my old approach was wrong what it did was it updated directly the force of the current body leaving lets say another body clueless of the old information think of it this way if i update earth position directly then mars is not being pulled by earth new position distance it should be pulled by the old one so the force wont be equal anymoe

--- Physics.py
+++ Physics.py
@@ -12,28 +12,12 @@ class Physics:
     @staticmethod
     def calculateForce(b1: Body, b2 : Body):
         # force on b1 by b2
-        m1 = b1.getMass()
+        m1 = b1.getMa3ss()
         m2 = b2.getMass()
         distance =  Vector2D(b1.x - b2.x, b1.y - b2.y)
         force = (Physics.G * m1 * m2 / (distance.calcMagnitude()**2)) * Physics.rVector(b1, b2)
         return force
 
-    @staticmethod
-    def calculateAcceleration(b1: Body, b2 : Body):
-        acceleration = b1.getAcceleration().add(Physics.calculateForce(b1, b2).scale(1/b1.getMass()))
-        b1.acceleration = acceleration
-        return acceleration
-
-    @staticmethod
-    def calculateVelocity(b1: Body, b2 : Body, dt):
-        velocity = b1.getVelocity().add(Physics.calculateAcceleration(b1, b2).scale(dt))
-        b1.velocity = velocity
-        return velocity
-    @staticmethod
-    def calculatePosition(b1: Body, b2 : Body, dt):
-        position = Physics.calculateVelocity(b1, b2, dt).scale(dt)
-        b1.position = position
-        return position
 
 
 
